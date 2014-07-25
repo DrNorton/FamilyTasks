313e6045e5fd6b1fa5432dc4d8d194f6522226e6
Andrew <andrey.kozak@altarix.com>
2014-07-25 18:09:29 +0400

    Небольшие ремарки

---
+++ FamilyTasks.Mobile/FamilyTasks.Mobile.Api/TestClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FamilyTasks.Mobile.Api
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+
+
+        }
+
+        public void TestMethod()
+        {
+       
+
+          
+        }
+
+        public class HelloRequest
+        {
+            public string Name { get; set; }
+        }
+
+        public class HelloResponse
+        {
+            public string Result { get; set; }
+        }
+    }
+}
