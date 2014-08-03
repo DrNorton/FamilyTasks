a130179ea1bcda72c81fdfc904c67ff1e09f449b
vgrachev2 <V_grachov@mail.ru>
2014-08-03 14:35:32 +0400

    dix

---
+++ FamilyTasks.Api/FamilyTasks.Dependencies.Castle/Installers/RepositoriesInstaller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using FamilyTasks.Dao.Repositories;
+using FamilyTasks.EfDao;
+
+namespace FamilyTasks.Dependencies.Castle.Installers
+{
+    public class RepositoriesInstaller: IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<EfContext>().LifestyleTransient());
+
+            container.Register(Component.For<IAuthRepository, EfAuthRepository>().LifestyleTransient());
+
+        }
+    }
+}
