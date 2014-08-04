e6b38f01abc2011429736a9a9c99f478f852b721
Andrew <andrey.kozak@altarix.com>
2014-08-04 19:47:20 +0400

    Добавил проект содержащий прототипы

---
+++ FamilyTasks.Api/FamilyTasks.Api/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using FamilyTasks.Api.ApiResults;
+using FamilyTasks.Api.Entities;
+using FamilyTasks.Dto.Users;
+using FamilyTasks.EfDao;
+using Microsoft.AspNet.Identity;
+
+namespace FamilyTasks.Api.Controllers
+{
+    [System.Web.Http.RoutePrefix("api/Account")]
+    public class AccountController : CustomApiController
+    {
+        private EfAuthRepository _repo = null;
+
+        public AccountController()
+        {
+            _repo = new EfAuthRepository();
+        }
+
+        // POST api/Account/Register
+        [System.Web.Http.AllowAnonymous]
+        [System.Web.Http.Route("Register")]
+        public async Task<IHttpActionResult> Register(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await _repo.RegisterUser(new CreateUserDto(userModel.Email, userModel.Email, string.Empty, userModel.Password));
+
+            IHttpActionResult errorResult = GetErrorResult(result);
+
+            if (errorResult != null)
+            {
+                return errorResult;
+            }
+
+            return EmptyApiResult();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ FamilyTasks.Api/FamilyTasks.Api/Entities/ProjectModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FamilyTasks.Api.Entities
+{
+    public class ProjectModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+    }
+}
